1deef5c8d6ff4de7add868bd21d8378b62fafd97
Adrien Leloup <adrien@whitecube.be>
2022-04-08 21:26:44 +0200

    Added post resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Whitecube',
+            'email' => 'adrien@whitecube.be',
+            'password' => bcrypt('test')
+        ]);
     }
 }
